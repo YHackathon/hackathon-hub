319c00d7e2bc5dfd538a22e8044984882f346687
Caelly <cellya.sirot@gmail.com>
2023-11-08 00:20:06 +0100

    feat(project): add all projects in json

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,46 @@
 <template>
    <v-container id="projects" class="listingProject">
+    <v-app>
+    <v-app-bar app color="indigo" dark>
+      <v-toolbar-title>Faux SoundPlayer</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <!-- Faux SoundPlayer Controls -->
+      <v-btn icon>
+        <v-icon>mdi-skip-previous</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-play</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-pause</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-skip-next</v-icon>
+      </v-btn>
+
+      <v-slider
+        class="mx-4"
+        v-model="soundLevel"
+        :max="100"
+        step="1"
+        hide-details
+        small
+      ></v-slider>
+
+      <v-btn icon @click="toggleMute">
+        <v-icon>{{ isMuted ? 'mdi-volume-off' : 'mdi-volume-high' }}</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </v-app>
+
+
+
+
         <v-card class="my-4">
             <v-card-title>Filtre selon le type de projet qui t'intéresse !</v-card-title>
             <v-card-text>
@@ -14,22 +55,11 @@
             <v-img :src="project.imageSrc" height="200px"></v-img>
             <v-card-title class="my-1">{{ project.name }}</v-card-title>
             <v-card-text>{{ project.description }}</v-card-text>
-            <v-card-subtitle class="mt-1">Technologies :</v-card-subtitle>
-            <v-chip v-for="tech in project.technologies" :key="tech" class='bg-mediumblue'>{{ tech }}</v-chip>
-            <v-btn width="100%" class="mt-3" color="deepblue" @click="showPopup(project)">
-            Voir les détails
+            <v-chip v-for="tech in project.technologies" :key="tech" class='bg-mediumblue mr-4'>{{ tech }}</v-chip>
+            <v-btn width="100%" class="mt-3" color="deepblue" :href="project.link">
+            Découvrir le projet
           </v-btn>
 
-          <v-dialog v-model="popupVisible" width="500px">
-            <v-card class="pa-2">
-              <v-img :src="currentProject.imageSrc" height="200px"></v-img>
-              <v-card-title class="my-3">{{ currentProject.name }}</v-card-title>
-              <v-card-text>{{ currentProject.longDescription }}</v-card-text>
-              <v-btn color="deepblue" class="mt-3" @click="popupVisible = false">
-                Fermer
-              </v-btn>
-            </v-card>
-          </v-dialog>
           </v-card>
         </v-col>
       </v-row>
@@ -40,112 +70,161 @@
   export default {
   data() {
     return {
-      filters: ['Web', 'Jeux', 'DevOps', 'C#','Docker', 'Java','Javascript', 'Golang', 'Laravel', 'MongoDB', 'MySQL', 'PHP','React', 'Unity','Vue.js'],
+      soundLevel:30, 
+      isMuted: false,
+      filters: ['Web', 'Jeu en ligne', 'Mobile', 'Jeu vidéo', 'Multijoueur','Logiciel'],
       projects: [
         {
           id: 1,
           name: 'YDiscover',
           link : 'https://main.dmnepnuwu4hll.amplifyapp.com/',
-          description: 'Création d\'un site de recommandations d\'artistes et génération de fond d\'écran.',
+          description: 'Création d\'un site de recommandations d\'artistes et génération de fond d\'écran en fonction des albums les plus streamés.',
           imageSrc: 'src/assets/bg_card/project_1.PNG',
-          longDescription:'Afin de perfectionner ma compréhension du PHP, de la POO et de MySQL, j’ai réalisé un site d’e-commerce en me basant sur le modèle MVC et en intégrant une API de paiement. Ce projet m’a permis notamment de mieux appréhender les fraweworks que sont Symfony et Laravel.',
           technologies: ['Web'],
         },
         {
           id: 2,
-          name: 'YFleet',
-          description: 'Création d\'une plateforme web de gestion de logistique et de transport',
-          longDescription:'Notre plateforme basée sur des micro services vise à automatiser et optimiser les processus de transport pour les entreprises. Avec notre plateforme, vous pouvez y planifier les itinéraires de manière efficace pour maximiser la productivité, gérer facilement votre flotte en temps réel, suivre les marchandises en toute transparence et réduire la charge de travail des équipes de transport',
-          imageSrc: 'https://picsum.photos/200/300?random=1',
-          technologies: ['React', 'Golang', 'MongoDB', 'Microservices', 'Docker','Développement web', 'DevOps'],
+          name: 'YMusic',
+          link : '/',
+          description: 'Création d\'un IDE pour jouer de la musique. Codez pour générer des sons qui seront interprétés par notre outil.',
+          imageSrc: 'https://www.optimoz.fr/wp-content/uploads/2018/05/404-not-found-error-.jpg',
+          technologies: ['Web'],
         },
         {
           id: 3,
-          name: 'Forum',
-          description: 'Création d\'un forum web reprenant les code des réseaux sociaux',
-          longDescription:'Dernier projet de ma première année d’études, j’ai pu mettre en application l’intégralité des connaissances acquises en Golang en recréant un équivalent de Twitter en quelques semaines.',
-          imageSrc: 'https://picsum.photos/200/300?random=1',
-          technologies: ['HTML', 'Golang', 'CSS', 'Javascript', 'Github', 'Travail en équipe','Développement web'],
+          name: 'YLocoTunes',
+          link : 'https://bechamelloww.github.io/vitrine-YLocoTunes/#mvpH',
+          description: 'Application web de découverte d\'artistes locaux et exploration des événements musicaux locaux.',
+          imageSrc: 'src/assets/bg_card/project_3.PNG',
+          technologies: ['Web'],
         },
         {
           id: 4,
-          name: 'Hangman Web',
-          description: 'Création d\'un forum web reprenant les code des réseaux sociaux',
-          longDescription:"Sur le même principe que le projet console, il fallait recréer le jeu du pendu mais cette fois-ci sur un format exécutable par un navigateur.",
-          imageSrc: 'https://picsum.photos/200/300?random=1',
-          technologies: ['HTML', 'Golang', 'CSS', 'Javascript', 'Github', 'Travail en équipe','Développement web'],
+          name: 'SoundSphere',
+          link : 'https://soundsphere.gcristini.fr/',
+          description: 'SoundSphere est une application mobile qui permet de créer une file d\'attente musicale collaborative.',
+          imageSrc: 'src/assets/bg_card/project_4.PNG',
+          technologies: ['Mobile', 'Multijoueur'],
         },
         {
           id: 5,
-          name: 'Groupie Tracker',
-          description: 'Création d\'un site de listing de musiques et `d\'artistes',
-          longDescription:'Pour mieux comprendre l’utilisation des API, j’ai créé un site web référençant les données d’une API de musiques fournie en local. À cela se rajoute l’API Spotify et ses données ce qui rend le site encore à jour au moment où vous lisez ces lignes. Les musiques affichées peuvent être lues par le biais d’un lecteur intégré et être recherchées par le biais d’une barre de recherche.',
-          imageSrc: 'https://picsum.photos/200/300?random=1',
-          technologies: ['HTML', 'Golang', 'CSS', 'Javascript','Github', 'Travail en équipe','Développement web'],
-        },
-        {
-          id: 5,
-          name: 'YStock',
-          description: 'Création d\'un logiciel de gestion de stock d\'une boutique en ligne',
-          longDescription:'Dans la continuité du site d’e-commerce réalisé en PHP, il nous a été demandé de réaliser un logiciel permettant de gérer les stocks de ladite boutique en ligne.',
-          imageSrc: 'https://picsum.photos/200/300?random=1',
-          technologies: ['C#', 'WPF', 'MySQL', 'Interfaçage', 'Développement logiciel'],
+          name: 'Swopy',
+          link : 'https://vitrine.swopy.fun/',
+          description: 'Application web qui permet de créer une file d\'attente musicale entre amis avec un système de vote par popularité.',
+          imageSrc: 'src/assets/bg_card/project_5.PNG',
+          technologies: ['Web', 'Multijoueur'],
         },
         {
           id: 6,
-          name: 'YGame',
-          description: 'Création d\'un jeu vidéo avec un système de leveling et de parcours de map',
-          longDescription:'Afin de découvrir le game programming nous nous sommes initier à Unity et avons dû créer un jeu. J\'ai pu appréhender l\'utilisation des touches pour me déplacer et la gestion des niveaux.',
-          imageSrc: 'https://picsum.photos/200/300?random=1',
-          technologies: ['C#', 'Unity', 'Game Programming', 'Interfaçage', 'Game Design','Développement logiciel'],
+          name: 'YQuizz',
+          link : 'https://hackathon-23-24-equipe6.github.io/vitrine//',
+          description: 'Application de quizz en ligne permettant de tester ses connaissances sur les différents genres musicaux.',
+          imageSrc: 'src/assets/bg_card/project_6.PNG',
+          technologies: ['Web', 'Jeu en ligne'],
         },
         {
           id: 7,
-          name: 'Hangman',
-          description: 'Création d\'un jeu du pendu avec gestion des différents statuts de partie',
-          longDescription:'Premier projet de ma scolarité, il m’a permis de mettre en exergue les différentes compétences techniques acquises durant les 30 jours d’apprentissage intensif du langage Golang.',
-          imageSrc: 'https://picsum.photos/200/300?random=1',
-          technologies: ['Golang', 'Serveur', 'Développement logiciel', 'Interfaçage', 'Game Design'],
+          name: 'Harmonifund',
+          link : 'https://vitrine-harmonifund.netlify.app/',
+          description: 'Participez aux campagnes de financement participatif pour soutenir vos artistes préférés !',
+          imageSrc: 'src/assets/bg_card/project_7.PNG',
+          technologies: ['Web'],
         },
         {
           id: 8,
-          name: 'Moteur de recherche de films',
-          description: 'Création d\'un système de moteur de recherche de films',
-          longDescription:'Afin de consolider nos acquis en Java, j’ai réalisé durant ma deuxième année une application de gestion de stocks. Celle-ci, reliée à une API, permettait de rechercher et d\'obtenir des détails d’ajouter sur les films et séries.',
-          imageSrc: 'https://picsum.photos/200/300?random=1',
-          technologies: ['Java', 'Programmation Orientée Objet', 'Développement logiciel', 'Design Pattern'],
+          name: 'QuizTune',
+          link : 'https://dancing-crumble-390602.netlify.app/',
+          description: 'Plateforme de quizz en temps réel en multijoueurs sous forme d\'extraits musicaux ou de lyrics à reconnaître.',
+          imageSrc: 'src/assets/bg_card/project_8.PNG',
+          technologies: ['Web', 'Jeu en ligne', 'Multijoueur'],
         },
         {
           id: 9,
-          name: 'Train',
-          description: 'Mise en place des différents design patterns pouvant servir à la gestion d\'un train',
-          longDescription:'Afin d’appréhender la programmation orientée objet, nous avons du créer une application mettant en application toutes les notions de la programmation orientée objet.Le projet doté d’un menu, permet de customiser entièrement un train : de la direction de celui-ci en passant par le nombre de wagons.',
-          imageSrc: 'https://picsum.photos/200/300?random=1',
-          technologies: ['Java', 'Programmation Orientée Objet', 'Développement logiciel', 'Design Pattern'],
+          name: 'OST-Museum',
+          link : 'https://ost-museum-web.vercel.app/',
+          description: 'Musée interactif où vous pourrez écouter les différents soundtracks de films, jeux vidéo et bien d\'autres encore !',
+          imageSrc: 'src/assets/bg_card/project_9.PNG',
+          technologies: ['Jeu vidéo'],
         },
         {
           id: 10,
-          name: 'CRUD Vue.JS / Laravel ',
-          description: 'Réalisation d\'un test technique mettant en oeuvre Vue 3 et une API sous Laravel',
-          longDescription:'Ce projet m\'a permis d\'apprendre à utiliser les technologies que sont Vue.JS et Laravel et à approndir mes connaissances en POO et surtout en développement front.',
-          imageSrc: 'https://picsum.photos/200/300?random=1',
-          technologies: ['Vue.JS', 'Laravel', 'Développement web', 'API', 'MySQL'],
+          name: 'SonoSync',
+          link : 'https://thomastiraboschi.github.io/Site-Vitrine-SonoSync/',
+          description: 'Incarnez un agent d\'entretien dans un labyrinthe de défis et d\'énigmes rythmé par la musique diffusée par un poste radio aux pouvoirs suprenants.',
+          imageSrc: 'src/assets/bg_card/project_10.PNG',
+          technologies: ['Jeu vidéo'],
         },
         {
           id: 11,
-          name: 'Simulation de feu de forêt',
-          description: 'Réalisation d\'un test technique permettant de simuler visuellement un feu de forêt',
-          longDescription:'Ce projet m\'a permis de faire du développement logiciel avec une réelle interface graphique, notion que je n\'avais pas pu aborder dans mon cursus scolaire.',
-          imageSrc: 'https://picsum.photos/200/300?random=1',
-          technologies: ['Java', 'JavaFX', 'Développement logiciel', 'Interfaçage', 'Algorithmie'],
+          name: 'BeatTheBeat',
+          link : 'https://hackathon-btb-vitrine.netlify.app/',
+          description: 'Testez vos connaissances à travers des énigmes sonores, défiez vos amis en temps réel et découvrez de nouveaux genres et artistes.',
+          imageSrc: 'src/assets/bg_card/project_11.PNG',
+          technologies: ['Web', 'Multijoueur', 'Jeu en ligne'],
         },
         {
           id: 12,
-          name: 'Equipe 17',
-          description: 'Réalisation d\'une application de babyfoot connecté',
-          longDescription:'Premier projet où j\'ai travaillé dans une équipe de 7 personnes. J\'y ai approfondi mes connaissances en Laravel',
-          imageSrc: 'https://picsum.photos/200/300?random=1',
-          technologies: ['Laravel', 'Développement web', 'Github', 'Travail en équipe'],
+          name: 'Solf\'ez',
+          link : 'https://annegnoemie.github.io/solfez/',
+          description: 'Vous avez toujours voulu apprendre à jouer au piano mais ne savez pas par où commencer ? Vous voulez améliorer votre oreille musicale ?',
+          imageSrc: 'src/assets/bg_card/project_12.PNG',
+          technologies: ['Web'],
+        },
+        {
+          id: 13,
+          name: 'Soundle',
+          link : 'https://gaspardcs.fr/pages/accueil.html',
+          description: 'Jeu vidéo explorant les différents gameplay d\'un jeu 2D en exploitant les effets sonores !',
+          imageSrc: 'https://www.optimoz.fr/wp-content/uploads/2018/05/404-not-found-error-.jpg',
+          technologies: ['Web', 'Jeu en ligne'],
+        },
+        {
+          id: 14,
+          name: 'YFestival',
+          link : '/',
+          description: 'Application web de gestion de festival avec informations sur les artistes et transports.',
+          imageSrc: 'https://www.optimoz.fr/wp-content/uploads/2018/05/404-not-found-error-.jpg',
+          technologies: ['Web'],
+        },
+        {
+          id: 15,
+          name: 'YMusic',
+          link : 'https://ymusic2023.netlify.app/',
+          description: 'Blind test en ligne sous la forme d\'un pendu. Tapez les lettres pour découvrir le son !',
+          imageSrc: 'src/assets/bg_card/project_15.PNG',
+          technologies: ['Web','Jeu en ligne'],
+        },
+        {
+          id: 16,
+          name: 'YError',
+          link : 'https://ynov-hackathon-groupe-16.netlify.app/',
+          description: 'Extension VS Code musicale de détection d\'erreurs de syntaxe sur différents langages.',
+          imageSrc: 'src/assets/bg_card/project_16.PNG',
+          technologies: ['Logiciel'],
+        },
+        {
+          id: 17,
+          name: 'MusiClash',
+          link : 'https://34.71.240.139/',
+          description: 'Application web de blind test en multijoueur local.',
+          imageSrc: 'src/assets/bg_card/project_17.PNG',
+          technologies: ['Web', 'Jeu en ligne', 'Multijoueur'],
+        },
+        {
+          id: 18,
+          name: 'Beathoven',
+          link : 'https://beathoven-bot.netlify.app/',
+          description: 'Bot Discord vous permettant de transformer vos messages en bande sonore.',
+          imageSrc: 'src/assets/bg_card/project_18.PNG',
+          technologies: ['Logiciel'],
+        },
+        {
+          id: 19,
+          name: 'Harmon IA',
+          link : 'https://kaleidoscopic-valkyrie-fb1af0.netlify.app/',
+          description: 'Application web d\'anlyse de fichiers musicaux et de retranscription de paroles de musique.',
+          imageSrc: 'src/assets/bg_card/project_19.PNG',
+          technologies: ['Web'],
         },
       ],
       activeFilters: [],
